74667f64eda0087bee7965860837641d67f391ea
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-23 14:28:10 +0100

    Further code for ATP clone v6

--- atp.tf
+++ atp.tf
@@ -12,6 +12,24 @@ resource "oci_database_autonomous_database" "FoggyKitchenATPdatabase" {
   subnet_id                = oci_core_subnet.FoggyKitchenATPEndpointSubnet.id      
 }
 
+resource "oci_database_autonomous_database" "FoggyKitchenATPdatabaseClone" {
+  source                   = "DATABASE"
+  source_id                = oci_database_autonomous_database.FoggyKitchenATPdatabase.id
+  clone_type               = "FULL"
+
+  admin_password           = var.atp_password
+  compartment_id           = oci_identity_compartment.FoggyKitchenCompartment.id
+  cpu_core_count           = var.FoggyKitchen_ATP_database_cpu_core_count
+  data_storage_size_in_tbs = var.FoggyKitchen_ATP_database_data_storage_size_in_tbs
+  db_name                  = var.FoggyKitchen_ATP_database_db_clone_name
+  display_name             = var.FoggyKitchen_ATP_database_display_clone_name
+  freeform_tags            = var.FoggyKitchen_ATP_database_freeform_tags
+  license_model            = var.FoggyKitchen_ATP_database_license_model
+  nsg_ids                  = [oci_core_network_security_group.FoggyKitchenATPSecurityGroup.id]   
+  private_endpoint_label   = var.FoggyKitchen_ATP_database_atp_clone_private_endpoint_label
+  subnet_id                = oci_core_subnet.FoggyKitchenATPEndpointSubnet.id      
+}
+
 data "oci_database_autonomous_databases" "FoggyKitchenATPdatabases" {
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   display_name = var.FoggyKitchen_ATP_database_display_name
@@ -28,23 +46,3 @@ output "FoggyKitchen_ATP_databases" {
 output "parallel_connection_string" {
   value = [lookup(oci_database_autonomous_database.FoggyKitchenATPdatabase.connection_strings.0.all_connection_strings, "PARALLEL", "Unavailable")]
 }
-
-resource "oci_database_autonomous_database" "FoggyKitchenATPdatabaseClone" {
-  source                   = "DATABASE"
-  source_id                = oci_database_autonomous_database.FoggyKitchenATPdatabase.id
-  clone_type               = "FULL"
-  
-  admin_password           = var.atp_password
-  compartment_id           = oci_identity_compartment.FoggyKitchenCompartment.id
-  cpu_core_count           = var.FoggyKitchen_ATP_database_cpu_core_count
-  data_storage_size_in_tbs = var.FoggyKitchen_ATP_database_data_storage_size_in_tbs
-  db_name                  = var.FoggyKitchen_ATP_database_db_clone_name
-  display_name             = var.FoggyKitchen_ATP_database_display_clone_name
-  freeform_tags            = var.FoggyKitchen_ATP_database_freeform_tags
-  license_model            = var.FoggyKitchen_ATP_database_license_model
-  nsg_ids                  = [oci_core_network_security_group.FoggyKitchenATPSecurityGroup.id]   
-  private_endpoint_label   = var.FoggyKitchen_ATP_database_atp_clone_private_endpoint_label
-  subnet_id                = oci_core_subnet.FoggyKitchenATPEndpointSubnet.id      
-}
-
-
